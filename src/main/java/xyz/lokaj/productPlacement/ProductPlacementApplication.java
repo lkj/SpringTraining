ac366ce1775d0f7bef0cdbb593ceaa20d88c7df6
Piotr Łokaj <piotrek.lokaj@gmail.com>
2022-05-01 07:30:20 +0200

    Init commit

---
+++ src/main/java/xyz/lokaj/productPlacement/ProductPlacementApplication.java
@@ -0,0 +1,13 @@
+package xyz.lokaj.productPlacement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPlacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPlacementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/lokaj/productPlacement/ProductPlacementApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.lokaj.productPlacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPlacementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
